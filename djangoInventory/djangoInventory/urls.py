0cadda5ebcdf10c69d2ae3726026e80066e3ccbf
SteelTitansax <manuel.portero.leiva@gmail.com>
2024-06-23 16:25:27 +0200

    Login code

--- djangoInventory/djangoInventory/urls.py
+++ djangoInventory/djangoInventory/urls.py
@@ -14,8 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth.views import LogoutView
 from django.urls import path
+from inventory import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',views.home, name='home'),
+    path('signup/',views.signup,name='signup'),
+    path('logout/', views.signout, name='logout'),
+    path('signin/',views.signin,name='signin'),
+
 ]
